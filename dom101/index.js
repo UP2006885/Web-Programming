7fadcae626d22ea25847b070a66231cb95b0e7b7
UP2006885 <91878094+UP2006885@users.noreply.github.com>
2022-02-10 12:33:45 +0000

    Updated JS

--- dom101/index.js
+++ dom101/index.js
@@ -36,5 +36,96 @@ function setClass(elem, str){
     return elem;
 }
 
+function addAClass(elem, str){
+    elem.classList.add(str);
+    return elem;
+}
 
-// add your functions here
+function removeAClass(elem, str){
+    elem.classList.remove(str);
+    return elem;
+}
+
+function newElement(name){
+    const q = document.createElement(name);
+    return q;
+
+}
+
+/** Accepts an ID(Without #) Returns first element that has a corresponding ID to the one passed.*/
+function findElementById(id){
+    const e = document.querySelector(`#${id}`);
+    return e;
+}
+
+function findElementsByQuery(query){
+    const e = document.querySelectorAll(query);
+    return e;
+
+}
+
+function reverseList(query){
+    let e = document.querySelector(query);
+    let amt = e.childNodes.length;
+
+    while(amt--){
+        e.appendChild(e.childNodes[amt]);
+    }
+    return e;
+}
+
+function mover(moveThis, appendToThis){
+    const e = document.querySelector(appendToThis);
+    e.append(document.querySelector(moveThis));
+}
+
+function filler(list, candidates){
+    let lis = [];
+
+    for(let i = 0; i < candidates.length; i++){
+        lis[i] = document.createElement("li");
+        lis[i].textContent = candidates[i];
+        list.append(lis[i]);
+    }
+}
+
+function dupe(selector){
+    const a = document.querySelector(selector);
+    const b = a.cloneNode();
+    b.textContent = a.textContent;
+
+    document.querySelector(selector).parentElement.append(b);
+}
+
+function removeAll(selector){
+    let a = [].slice.call(document.querySelectorAll(selector));
+    for(let i  = 0; i < a.length; i++){
+        a[i].remove();
+    }
+}
+
+function getUserData(){
+    // #speed, #student form input fields. The result should be an object like this: { name: 'john', speed: 30, student: true }.
+    let userData = ["username", "speed", "student",];
+    let userValues = [];
+    let x = {};
+
+    for(let i = 0; i < userData.length; i++){
+        if(userData[i] === "student"){
+            userValues[i] = document.querySelector(`#${userData[i]}`).checked;
+        }else if(userData[i] === "speed"){
+            userValues[i] = parseInt(document.querySelector(`#${userData[i]}`).getAttribute("value"));
+        }else{
+            userValues[i] = document.querySelector(`#${userData[i]}`).getAttribute("value");
+        }
+    }
+    // userData.forEach((userData, i) => x[userData] = userValues[i]); // Stores the arrays into a single object.
+    userData[0] = "name";
+    for(let i = 0; i < userData.length; i++){
+        x[userData[i]] = userValues[i];
+    }
+    console.log(x);
+    return x;
+
+    
+}
